318de54d23dbba9bc23f1f01658a75dc41d366b0
caiopapao <109988548+caiopapao@users.noreply.github.com>
2023-01-05 20:27:51 -0300

    Se Deus quiser vai funcionar
    Amém

---
+++ index.js
@@ -0,0 +1,16 @@
+$('#card1').click(function() {
+  $(this).toggleClass('flipped');
+});
+
+$('#card2').click(function() {
+  $(this).toggleClass('flipped');
+});
+
+$('#card3').click(function() {
+  $(this).toggleClass('flipped');
+});
+
+$('#card4').click(function() {
+  $(this).toggleClass('flipped');
+});
+
